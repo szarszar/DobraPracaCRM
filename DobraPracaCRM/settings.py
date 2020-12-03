00943fd6ee928f3b53903fd1d32cb6e947ef2638
Michał Szarpak <michal.szar@gmail.com>
2020-12-04 00:23:20 +0100

    fucking with DB authentication

--- DobraPracaCRM/settings.py
+++ DobraPracaCRM/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'utf8'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
